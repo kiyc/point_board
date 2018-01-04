0c19b60318e48e39df9c790f7e27adf6b5bafe19
kiyc <kiyocct@gmail.com>
2018-01-04 22:38:36 +0900

    modify a point dialog

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,31 @@
     <div id="app">
         <h1>{{ title }}</h1>
         <table>
-            <user v-for="user in users" v-bind:user="user" v-bind:key="user.name"></user>
+            <user v-for="user in users" v-bind:user="user" v-bind:key="user.name" v-on:openPointDialog="openPointDialog"></user>
         </table>
         <h3>
             <total v-bind:total="total"></total>
         </h3>
+            <el-dialog
+                title="Please input point and password"
+                :visible.sync="pointDialogVisible"
+                width="30%"
+                :before-close="closePointDialog">
+                <table>
+                    <tr>
+                        <td>Point</td>
+                        <td><el-input v-model="point" type="text" placeholder="0" class="input_point"></el-input></td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td><el-input v-model="password" type="password" class="password"></el-input></td>
+                    </tr>
+                </table>
+                <span slot="footer" class="dialog-footer">
+                    <el-button v-on:click="closePointDialog">Cancel</el-button>
+                    <el-button type="primary" v-on:click="updatePoint">Submit</el-button>
+                </span>
+            </el-dialog>
         <signup></signup>
     </div>
 </template>
@@ -15,6 +35,7 @@
 import User from './User.vue'
 import Total from './Total.vue'
 import Signup from './Signup.vue'
+import { Message } from 'element-ui'
 const config = require('./config.js')
 window.config = config
 
@@ -24,7 +45,11 @@ export default {
     data () {
         return {
             title: config.TITLE,
-            users: []
+            users: [],
+            pointDialogVisible: false,
+            targetUserId: 0,
+            point: 0,
+            password: ''
         }
     },
     created: function() {
@@ -43,6 +68,56 @@ export default {
                     return elm
                 })
             });
+        },
+        openPointDialog: function(userId) {
+            this.targetUserId = userId,
+            this.pointDialogVisible = true
+        },
+        closePointDialog: function() {
+            this.targetUserId = 0
+            this.point = 0
+            this.password = ''
+            this.pointDialogVisible = false
+        },
+        updatePoint: function() {
+            const point = Number(this.point)
+
+            if (!point) return
+
+            let formdata = new FormData()
+            formdata.append('id', this.targetUserId)
+            formdata.append('password', this.password)
+            formdata.append('point', point)
+
+            this.closePointDialog()
+
+            fetch(
+                // update point
+                config.API_BASE_URL + '/users/update.php',
+                { method: 'POST', body: formdata }
+            ).then(response => {
+                return response.text()
+            }).then(text => {
+                const status = Number(text)
+                if (isNaN(status) || status) {
+                    throw new Error(text)
+                }
+                return status
+            }).then(status => {
+                // fetch users
+                this.fetchUsers()
+                Message({
+                    showClose: true,
+                    message: 'Success update point.',
+                    type: 'success',
+                })
+            }).catch(error => {
+                Message({
+                    showClose: true,
+                    message: '' + error,
+                    type: 'error'
+                })
+            })
         }
     },
     computed: {
@@ -87,5 +162,9 @@ td {
 
 a {
     color: #42b983;
+}
+
+.input_point {
+    width: 80px;
 }
 </style>
--- src/User.vue
+++ src/User.vue
@@ -3,102 +3,20 @@
         <td>{{ user.name }}: {{ user.point }}</td>
         <td>
             <el-button icon="el-icon-plus" v-on:click="openDialog"></el-button>
-            <el-dialog
-                title="Please input point and password"
-                :visible.sync="dialogVisible"
-                width="30%"
-                :before-close="closeDialog">
-                <table>
-                    <tr>
-                        <td>Point</td>
-                        <td><el-input v-model="point" type="text" placeholder="0" class="input_point"></el-input></td>
-                    </tr>
-                    <tr>
-                        <td>Password</td>
-                        <td><el-input v-model="password" type="password" class="password"></el-input></td>
-                    </tr>
-                </table>
-                <span slot="footer" class="dialog-footer">
-                    <el-button v-on:click="closeDialog">Cancel</el-button>
-                    <el-button type="primary" v-on:click="updatePoint">Submit</el-button>
-                </span>
-            </el-dialog>
         </td>
     </tr>
 </template>
 
 <script>
-import { Message } from 'element-ui'
 export default {
     props: ['user'],
-    data () {
-        return {
-            point: 0,
-            dialogVisible: false,
-            password: ''
-        }
-    },
     methods: {
         openDialog: function() {
-            this.dialogVisible = true
-        },
-        updatePoint: function() {
-            const point = Number(this.point)
-
-            if (!point) return
-
-            let formdata = new FormData()
-            formdata.append('id', this.user.id)
-            formdata.append('password', this.password)
-            formdata.append('point', point)
-
-            this.closeDialog()
-
-            fetch(
-                // update point
-                config.API_BASE_URL + '/users/update.php',
-                { method: 'POST', body: formdata }
-            ).then(response => {
-                return response.text()
-            }).then(text => {
-                const status = Number(text)
-                if (isNaN(status) || status) {
-                    throw new Error(text)
-                }
-                return status
-            }).then(status => {
-                // fetch user
-                fetch(
-                    config.API_BASE_URL + '/users/first.php?id=' + this.user.id
-                ).then(response => {
-                    return response.json()
-                }).then(json => {
-                    this.user.point = Number(json.point)
-                    Message({
-                        showClose: true,
-                        message: 'Success update point.',
-                        type: 'success',
-                    })
-                })
-            }).catch(error => {
-                Message({
-                    showClose: true,
-                    message: '' + error,
-                    type: 'error'
-                })
-            })
-        },
-        closeDialog: function() {
-            this.point = 0
-            this.password = ''
-            this.dialogVisible = false
+            this.$emit('openPointDialog', this.user.id)
         }
     }
 }
 </script>
 
 <style>
-.input_point {
-    width: 80px;
-}
 </style>
